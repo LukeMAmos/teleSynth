50efe39a4fe0376f38376ada1bd948fc8d6b8d02
LukeMAmos <lukeamos2111@gmail.com>
2026-09-17 11:31:45 +0100

    Draggable component implementation

---
+++ Source/UI/DraggableComponent/DraggableComponent.cpp
@@ -0,0 +1,68 @@
+#include "DraggableComponent.h"
+
+DraggableComponent::DraggableComponent(){
+    
+    setWantsKeyboardFocus(true);
+    
+    setDiameter(50.0f);
+}
+
+void DraggableComponent::paint(juce::Graphics &g){
+    
+    g.setColour (juce::Colours::blueviolet);
+
+    g.fillEllipse(0, 0, diameter, diameter);
+}
+
+void DraggableComponent::mouseDown(const juce::MouseEvent &event){
+    
+    dragger.startDraggingComponent(this, event);
+
+    
+}
+
+void DraggableComponent::mouseDrag(const juce::MouseEvent &event){
+    
+    dragger.dragComponent(this, event, nullptr);
+
+}
+
+void DraggableComponent::mouseUp(const juce::MouseEvent &event){
+
+    
+}
+
+
+void DraggableComponent::resized(){
+
+}
+
+void DraggableComponent::setDiameter(float diameterIn){
+    
+    diameter = diameterIn;
+}
+
+float DraggableComponent::getDiameter(){
+    
+    return diameter;
+}
+
+void DraggableComponent::setStartFunction(std::function<void(float xpos,float ypos)> function){
+    
+    onDragStart = function;
+}
+void DraggableComponent::setMoveFunction(std::function<void(float xpos,float ypos)> function){
+    
+    onDragMove = function;
+}
+void DraggableComponent::setEndFunction(std::function<void()> function){
+    
+    onDragEnd = function;
+    
+}
+
+void DraggableComponent::setXYBounds(juce::Rectangle<int> bounds){
+    
+    
+    
+}
---
+++ Source/UI/DraggableComponent/DraggableComponent.h
@@ -0,0 +1,52 @@
+#include <JuceHeader.h>
+
+class DraggableComponent : public juce::Component
+{
+    
+public:
+    DraggableComponent();
+    
+    void paint(juce::Graphics& g )override;
+    
+    void resized() override;
+    
+    //Dragging functions 
+    void mouseDown(const juce::MouseEvent& event) override ;
+       
+    void mouseDrag(const juce::MouseEvent& event) override ;
+       
+    void mouseUp(const juce::MouseEvent& event)override;
+    
+    //Get the position of the circle
+    float getPositionXCentre(){return (float)getX() + diameter / 2.0f;}
+    float getPositionYCentre(){return (float)getY() + diameter / 2.0f;}
+    
+    bool mDown = false;
+    
+    //Set and get the size of the circle
+    void setDiameter(float diameterIn);
+    float getDiameter();
+    
+    void setStartFunction(std::function<void(float xpos,float ypos)> function);
+    void setMoveFunction(std::function<void(float xpos,float ypos)> function);
+    void setEndFunction(std::function<void()> function);
+    
+    void callOnDragStart(float xpos,float ypos){onDragStart(xpos , ypos);}
+    void callOnDragMove(float xpos,float ypos){onDragMove(xpos , ypos);}
+    void callOnDragEnd(){onDragEnd();}
+    
+    //Function for setting the area the slider can be dragged inside
+    void setXYBounds(juce::Rectangle<int> bounds);
+    
+private:
+    
+    float diameter = 100;
+    
+    juce::ComponentDragger dragger;
+    
+    //Functions to call
+    std::function<void(float xpos,float ypos)> onDragStart;
+    std::function<void(float xpos,float ypos)> onDragMove;
+    std::function<void()> onDragEnd;
+};
+
